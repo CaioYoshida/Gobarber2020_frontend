dd5cf53905ea8608901d3bfaaad9ec65e04bf37c
Caio Yoshida Dell G3 <caio.yoshida@yahoo.com.br>
2021-03-07 12:01:58 -0300

    Updates

---
+++ precache-manifest.7f6670bf4181d52a48c0db96cb2e3fff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5bd2f78ad97182643c8b790277a3c12",
+    "url": "/Gobarber2020_frontend/index.html"
+  },
+  {
+    "revision": "30b272069e8ff1eccae0",
+    "url": "/Gobarber2020_frontend/static/js/2.8fbcb884.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/Gobarber2020_frontend/static/js/2.8fbcb884.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05c44fbf76dab4a0eb59",
+    "url": "/Gobarber2020_frontend/static/js/main.542e64a3.chunk.js"
+  },
+  {
+    "revision": "6f36b5e413f8d357ebb1",
+    "url": "/Gobarber2020_frontend/static/js/runtime-main.7253a87a.js"
+  },
+  {
+    "revision": "a49b07ec538713cf0e3910693544c7be",
+    "url": "/Gobarber2020_frontend/static/media/logo.a49b07ec.svg"
+  },
+  {
+    "revision": "6eac314f78fe224c4d769fb646523773",
+    "url": "/Gobarber2020_frontend/static/media/sign-in-background.6eac314f.png"
+  },
+  {
+    "revision": "c4d8f988b9c6499d56702a398b2f3d8b",
+    "url": "/Gobarber2020_frontend/static/media/sign-up-background.c4d8f988.png"
+  }
+]);
